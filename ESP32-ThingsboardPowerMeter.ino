6e6c3e14376e3caae36ee936e92bea8c68a0acc7
sirapol <s.sirapol@hotmail.com>
2021-11-08 11:39:12 +0700

    PZEM bug fix , add DHT22

--- ESP32-ThingsboardPowerMeter.ino
+++ ESP32-ThingsboardPowerMeter.ino
@@ -1,24 +1,34 @@
+#include "DHT.h"
+
 #define RXD2 16
 #define TXD2 17
 
 #define SDA_OLED 5
 #define SCL_OLED 4
 
+#define DHTPIN 5
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
 
 int requestCount = 0;
 unsigned long timeOut = millis();
 uint8_t bufferModbus[25];
 
-void setup() {
+void setup(;)
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  while (!Serial2);
-  Serial.println("Fin setup");
+  while (!Serial2)
+    ;
 
+  dht.begin();
+  Serial.println("Fin setup");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   if (millis() - timeOut > 2000)
   {
@@ -34,7 +44,6 @@ void loop() {
   }
   if (Serial2.available())
   {
-    Serial.println("data");
     for (int i = 0; i < 25; i++)
     {
       if (!Serial2.available())
@@ -43,13 +52,13 @@ void loop() {
         break;
       }
       bufferModbus[i] = Serial2.read();
+      delay(20);
     }
-
 
     uint32_t voltage = (uint32_t)bufferModbus[3] << 8 | (uint32_t)bufferModbus[4];
     uint32_t current = (uint32_t)bufferModbus[5] << 8 | (uint32_t)bufferModbus[6] | (uint32_t)bufferModbus[7] << 24 | (uint32_t)bufferModbus[8] << 16;
-    uint32_t power =   (uint32_t)bufferModbus[9] << 8 | (uint32_t)bufferModbus[10] | (uint32_t)bufferModbus[11] << 24 | (uint32_t)bufferModbus[12] << 16;
-    uint32_t energy =  (uint32_t)bufferModbus[13] << 8 | (uint32_t)bufferModbus[14] | (uint32_t)bufferModbus[15] << 24 | (uint32_t)bufferModbus[16] << 16;
+    uint32_t power = (uint32_t)bufferModbus[9] << 8 | (uint32_t)bufferModbus[10] | (uint32_t)bufferModbus[11] << 24 | (uint32_t)bufferModbus[12] << 16;
+    uint32_t energy = (uint32_t)bufferModbus[13] << 8 | (uint32_t)bufferModbus[14] | (uint32_t)bufferModbus[15] << 24 | (uint32_t)bufferModbus[16] << 16;
     uint32_t frequecy = (uint32_t)bufferModbus[17] << 8 | (uint32_t)bufferModbus[18];
 
     float fVoltage = voltage * 0.1;
@@ -57,13 +66,23 @@ void loop() {
     float fPower = power * 0.1;
     float fEnergy = energy * 0.001;
     float fFrequency = frequecy * 0.1;
-    
-    Serial.println(fVoltage);
-    Serial.println(fCurrent);
-    Serial.println(fPower);
-    Serial.println(fEnergy);
-    Serial.println(fFrequency);
-    
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    if (isnan(h) || isnan(t))
+    {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+    Serial.println("------------------------------");
+    Serial.println("Voltage = " + String(fVoltage));
+    Serial.println("Current = " + String(fCurrent));
+    Serial.println("Power   = " + String(fPower));
+    Serial.println("Energy  = " + String(fEnergy));
+    Serial.println("Freq    = " + String(fFrequency));
+    Serial.println("Humid   = " + String(h));
+    Serial.println("Temp    = " + String(t));
+
     while (Serial2.available())
     {
       Serial2.read();
